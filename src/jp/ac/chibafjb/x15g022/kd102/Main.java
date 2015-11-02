074ce3eccd87c72c6eaf90b3adfae2a198a91660
unknown <x15g022@PC504N005.chiba-fjb.ac.jp>
2015-11-02 14:40:26 +0900

    課題２つ目

---
+++ src/jp/ac/chibafjb/x15g022/kd102/Main.java
@@ -0,0 +1,18 @@
+package jp.ac.chibafjb.x15g022.kd102;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("1 船橋 太郎");
+		System.out.println("2 船橋 太郎");
+		System.out.println("3 船橋 太郎");
+		System.out.println("4 船橋 太郎");
+		System.out.println("5 船橋 太郎");
+		System.out.println("6 船橋 太郎");
+		System.out.println("7 船橋 太郎");
+		System.out.println("8 船橋 太郎");
+		System.out.println("9 船橋 太郎");
+		System.out.println("10 船橋 太郎");
+	}
+
+}
